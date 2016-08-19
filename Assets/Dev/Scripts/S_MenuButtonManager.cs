50cc51d868a4863265babe8edcadc538e014bfa4
Shadowsun <wendyfreedom31@hotmail.com>
2016-08-19 14:12:57 +0200

    menu -> manager done

--- Assets/Dev/Scripts/S_MenuButtonManager.cs
+++ Assets/Dev/Scripts/S_MenuButtonManager.cs
@@ -1,35 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class S_MenuButtonManager {
+public class S_MenuButtonManager : MonoBehaviour {
 
-    public static void Menu_Start()
+    public void Menu_Start()
+    {
+        // S_SceneManager.Load_Demo();
+        Debug.Log( "Click Start" );
+    }
+
+    public void Menu_Continue()
     {
         // S_SceneManager.Load_Demo();
         Debug.Log( "Click Continue" );
     }
 
-    public static void Menu_Continue()
-    {
-        // S_SceneManager.Load_Demo();
-        Debug.Log( "Click Continue" );
-    }
-
-    public static void Menu_Settings()
+    public void Menu_Settings()
     {
         // S_SceneManager.Load_Settings();
-        Debug.Log( "Click Continue" );
+        Debug.Log( "Click Settings" );
     }
 
-    public static void Menu_Credits()
+    public void Menu_Credits()
     {
         // S_SceneManager.Load_Credits();
-        Debug.Log( "Click Continue" );
+        Debug.Log( "Click Credits" );
     }
 
-    public static void Menu_Quit()
+    public void Menu_Quit()
     {
-        Debug.Log( "Click Continue" ); 
+        Debug.Log( "Click Quit" ); 
     }
 
 	
