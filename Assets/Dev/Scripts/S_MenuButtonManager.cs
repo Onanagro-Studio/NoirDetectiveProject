49026527332adacb94e4933f2b968becd3713f20
Shadowsun <wendyfreedom31@hotmail.com>
2016-08-19 12:18:46 +0200

    test slack bot

--- Assets/Dev/Scripts/S_MenuButtonManager.cs
+++ Assets/Dev/Scripts/S_MenuButtonManager.cs
@@ -5,28 +5,31 @@ public class S_MenuButtonManager {
 
     public static void Menu_Start()
     {
-        S_SceneManager.Load_Demo();
-
+        // S_SceneManager.Load_Demo();
+        Debug.Log( "Click Continue" );
     }
 
     public static void Menu_Continue()
     {
-
+        // S_SceneManager.Load_Demo();
+        Debug.Log( "Click Continue" );
     }
 
     public static void Menu_Settings()
     {
-
+        // S_SceneManager.Load_Settings();
+        Debug.Log( "Click Continue" );
     }
 
     public static void Menu_Credits()
     {
-
+        // S_SceneManager.Load_Credits();
+        Debug.Log( "Click Continue" );
     }
 
     public static void Menu_Quit()
     {
-
+        Debug.Log( "Click Continue" );
     }
 
 	
