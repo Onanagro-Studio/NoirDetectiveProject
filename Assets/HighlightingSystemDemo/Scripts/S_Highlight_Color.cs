428071ab5530f07a03ecd72a23e2ba7c0b1cca60
Shadowsun <wendyfreedom31@hotmail.com>
2016-08-30 10:14:03 +0200

    Hightlighting

---
+++ Assets/HighlightingSystemDemo/Scripts/S_Highlight_Color.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using HighlightingSystem;
+
+public class S_Highlight_Color : MonoBehaviour {
+
+    protected Highlighter h;
+    public Color m_color;
+
+    void Awake()
+    {
+        h = gameObject.AddComponent<Highlighter>();
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        h.ConstantOn( m_color );
+        h.SeeThroughOn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
