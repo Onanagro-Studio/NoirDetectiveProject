27655db9c777abffc4e25eb01ccb57db0ee2b923
Shadowsun <wendyfreedom31@hotmail.com>
2016-09-08 09:00:18 +0200

    Petites corrections

--- Assets/Dev/Scripts/S_SceneManager.cs
+++ Assets/Dev/Scripts/S_SceneManager.cs
@@ -6,21 +6,38 @@ class S_SceneManager
 {
     public static int CurrentScene = 0;
 
-    public static void Load_SplashScreen()
+    public static void Init_All()
     {
         S_AudioManager.Init_Vol();
+
+        // TODO: 
+        //          Init_Video
+        //          Init_Inputs
+    }
+
+    public static void Load_SplashScreen()
+    {
+        Init_All();
+
         Load_Scene( 0 );
     }
 
     public static void Load_Menu()
     {
-        S_AudioManager.Init_Vol();
+      
         Load_Scene( 1 );
     }
 
     public static void Load_Demo()
     {
         Load_Scene( 2 );
+    }
+
+    public static void Load_GameOver()
+    {
+        Load_Menu();
+
+
     }
 
     public static void Load_Next()
