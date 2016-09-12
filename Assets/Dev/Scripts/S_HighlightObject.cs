38834ce7a7b7c1f4d3ef74957b88c42a0c09e8c3
Shadowsun <wendyfreedom31@hotmail.com>
2016-09-12 23:29:00 +0200

    intégration LD Tuto => corrections, Demo => intégration complète

---
+++ Assets/Dev/Scripts/S_HighlightObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using HighlightingSystem;
+
+public class S_HighlightObject : MonoBehaviour {
+
+    public bool isHighlighted = true;
+    public Color m_HighlightColor = Color.gray;
+    public GameObject m_Object;
+    // Use this for initialization
+    void Start () {
+
+        m_highlighter = m_Object.AddComponent<Highlighter>();
+
+        if(isHighlighted)
+            m_highlighter.ConstantOnImmediate( m_HighlightColor );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if( isHighlighted )
+            m_highlighter.ConstantOnImmediate( m_HighlightColor );
+    }
+
+    private Highlighter m_highlighter;
+}
