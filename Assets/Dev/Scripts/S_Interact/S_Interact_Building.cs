9ce6c2f4dba830b4bb1cca4ac2d16ce72763e8d3
Shadowsun <wendyfreedom31@hotmail.com>
2016-09-12 15:46:38 +0200

    voila trytry

---
+++ Assets/Dev/Scripts/S_Interact/S_Interact_Building.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_Interact_Building : S_Interact
+{
+    //[HideInInspector]
+    public GameObject m_PortalTop, m_PortalBottom;
+
+    void Start()
+    {
+        Interact_Init();
+
+        m_PortalTopTransform = m_PortalTop.GetComponent<Transform>();
+        m_PortalBottomTransform = m_PortalBottom.GetComponent<Transform>();
+    }
+
+    protected override void On_Interact_Start( Collider _collision )
+    {
+        m_charact_controller.IsHidden = true;
+        //m_charact_controller.IsClimbing = true;
+
+        if( m_CharTransform.position.z > m_PortalTopTransform.position.z )
+        {
+            m_CharTransform.position = new Vector3( m_CharTransform.position.x, m_CharTransform.position.y, m_PortalTopTransform.position.z );
+
+            m_charact_controller.HighlightColor = new Color( 0, 0, 0, 0 );
+        }
+        else if( m_CharTransform.position.z < m_PortalBottomTransform.position.z )
+        {
+            m_CharTransform.position = new Vector3( m_CharTransform.position.x, m_CharTransform.position.y, m_PortalBottomTransform.position.z );
+
+            m_charact_controller.HighlightColor = new Color( 1, 1, 1, 1 );
+        }
+    }
+
+    protected override void On_Interact_Leave( Collider _collision )
+    {
+        m_charact_controller.IsHidden = false;
+        //m_charact_controller.IsClimbing = false;
+    }
+
+    public Transform m_PortalTopTransform, m_PortalBottomTransform;
+}
