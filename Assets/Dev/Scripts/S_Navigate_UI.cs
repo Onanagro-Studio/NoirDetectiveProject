25371668e51219a2136c681f90dddbca7df968a5
GaraQuor <delhaye.trystan@gmail.com>
2016-09-20 16:28:17 +0200

    hemlloo

---
+++ Assets/Dev/Scripts/S_Navigate_UI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class S_Navigate_UI : MonoBehaviour
+{
+    void Start()
+    {
+        m_buttonList = GetComponentsInChildren<Button>();
+
+        Select( 0 );
+    }
+    
+
+	void OnEnable ()
+    {
+        Select( 0 );
+    }
+
+    private void Select(int _id)
+    {
+        if (m_buttonList != null)
+        {
+            if( _id < 0 )
+                _id = m_buttonList.Length - 1;
+            else
+            if( _id >= m_buttonList.Length )
+                _id = 0;
+
+            m_currentId = _id;
+
+            m_buttonList[ m_currentId ].Select();
+        }
+    }
+
+    private int m_currentId;
+    private Button[] m_buttonList;
+}
