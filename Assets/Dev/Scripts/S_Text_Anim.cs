1b4871f3a5db997dfc7f3f4ea208c5a8ac1877a1
GaraQuor <delhaye.trystan@gmail.com>
2016-09-21 15:11:14 +0200

    La-baaaaas ! Dans les etoileeesss ...

---
+++ Assets/Dev/Scripts/S_Text_Anim.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_Text_Anim : MonoBehaviour
+{
+    public float m_speed = 80.0f;
+
+	void Start ()
+    {
+        m_transform = GetComponent<Transform>();
+    }
+	
+	void Update ()
+    {
+        m_transform.position += m_transform.up * m_speed * Time.deltaTime;
+	}
+
+    private Transform m_transform;
+}
