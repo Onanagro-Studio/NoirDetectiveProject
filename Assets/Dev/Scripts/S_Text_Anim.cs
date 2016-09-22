0787c7f4d0f74dc40f374ed3099b311fe674d92c
GaraQuor <delhaye.trystan@gmail.com>
2016-09-22 10:33:48 +0200

    Coucou on est mort :/

--- Assets/Dev/Scripts/S_Text_Anim.cs
+++ Assets/Dev/Scripts/S_Text_Anim.cs
@@ -4,16 +4,46 @@ using System.Collections;
 public class S_Text_Anim : MonoBehaviour
 {
     public float m_speed = 80.0f;
-
-	void Start ()
+    public S_MenuButtonManager m_manager;
+    
+    void Start ()
     {
-        m_transform = GetComponent<Transform>();
+        Reset();
+    }
+
+    void OnEnable()
+    {
+        Reset();
     }
 	
 	void Update ()
     {
-        m_transform.position += m_transform.up * m_speed * Time.deltaTime;
-	}
+        if( m_scroll && Time.realtimeSinceStartup > m_startTimer )
+        {
+            if( m_scroll )
+            {
+                m_transform.position += m_transform.up * m_speed * Time.deltaTime;
 
+                if( m_transform.position.y > 2589.0f )
+                    m_scroll = false;
+            }
+        }
+
+        if( Input.GetButtonDown( "Joy0_A" ) )
+            m_manager.Credits_Back();
+    }
+
+    private void Reset()
+    {
+        if( !m_transform )
+            m_transform = GetComponent<Transform>();
+
+        m_transform.position = new Vector3( m_transform.position.x, 0, m_transform.position.z );
+        m_scroll = true;
+        m_startTimer = Time.realtimeSinceStartup + 2.0f;
+    }
+
+    private float m_startTimer;
+    private bool m_scroll;
     private Transform m_transform;
 }
